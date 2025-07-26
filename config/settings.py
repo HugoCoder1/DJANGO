e6f84a2fc39415dca40f01f07306ceab032f207d
HuguesApithy <hapithy@niyiexpertise.com>
2025-07-26 16:33:41 +0100

    update

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-a2530k(qk1poz$@0e854n$_-(&+0l#u=#-*msb5e=-c1dfvkr6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
